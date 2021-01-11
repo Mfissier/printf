e2d6ae3c603785c40b7fdfcf0d158523db8ead26
user42 <mbfissier@gmail.com>
2021-01-11 15:46:44 +0100

    Makefile uplauded

---
+++ Makefile
@@ -0,0 +1,5 @@
+NAME = libftprintf.a
+
+FLAG = -Wall -Wextra -Werror
+
+SRCS = ./srcs/ft_putstr_test.c
---
+++ ft_printf.c
@@ -0,0 +1,13 @@
+#include "ft_printf.h"
+
+int ft_printf(const char *str, ...)
+{
+	str = NULL;
+	return(0);
+}
+
+int main(void)
+{
+ft_putstr_test("bonjour\n");	
+	return(0);
+}
